e1ecf6e4210a13800de14a4c93eea46e49286aaa
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2024-09-20 14:54:03 +0200

    Add sample code

---
+++ 24_09_20/more_turtles.py
@@ -0,0 +1,13 @@
+import turtle
+import random
+from typing import List
+
+screen = turtle.Screen()
+screen.setup(1920,1080)
+
+pepa = turtle.Turtle()
+vlasta = turtle.Turtle()
+
+for i in range(100):
+    pepa.forward(100)
+    vlasta.left(90)
---
+++ 24_09_20/opila_zelva.py
@@ -0,0 +1,7 @@
+import turtle 
+
+while True:
+    turtle.left(90)
+    turtle.forward(100)
+
+turtle.mainloop()
