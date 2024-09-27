7e130801565276986b17fd83841a142392fe9d1b
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2024-09-27 14:02:26 +0200

    Edit task files

--- 24_09_20/more_turtles.py
+++ 24_09_20/more_turtles.py
@@ -1,13 +1,18 @@
 import turtle
-import random
-from typing import List
 
+# Setup screen first
 screen = turtle.Screen()
 screen.setup(1920,1080)
 
+# Save each turtle into a variable
 pepa = turtle.Turtle()
 vlasta = turtle.Turtle()
 
+# Control each turtle by using its variable
 for i in range(100):
     pepa.forward(100)
     vlasta.left(90)
+
+# TODO: Make the turtles have different color
+# TODO: Make them walk in random directions (sometimes they turn, sometimes they just go forward)
+# TODO: Make the turtles fast
--- 24_09_20/opila_zelva.py
+++ 24_09_20/opila_zelva.py
@@ -1,7 +1,9 @@
 import turtle 
+import random 
 
+# TODO: Save a random number to variable with variable = random.randint(a,b)
+# TODO: Decide where the turtle should turn based on what is stored in the variable (use if statement)
+
+# While True makes a forever loop
 while True:
-    turtle.left(90)
-    turtle.forward(100)
-
-turtle.mainloop()
+    ... # move the turtle somehow
