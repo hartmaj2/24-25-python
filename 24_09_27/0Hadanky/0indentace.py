849c9341dfba69cbcbb653caaad9820c67794db7
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2024-09-27 14:21:14 +0200

    Add files

---
+++ 24_09_27/0Hadanky/0indentace.py
@@ -0,0 +1,15 @@
+# Co je v nasledujicim kodu za chybu?
+
+character = "Kirby"
+health = 100
+print(f"Character: {character}")
+print(f"Starting Health: {health}")
+
+# Check if the character has enough health
+if health > 50:
+    print("You are in great shape!")
+print("With this much HP you can go fight a dragon!")
+if health <= 50:
+    print("You need to find a health pack!")  # This line is not indented correctly
+
+print("Let's start the adventure!")
---
+++ 24_09_27/0Hadanky/1promenna_vs_retezec.py
@@ -0,0 +1,6 @@
+# Jaky je rozdil mezi nasledujicimi
+
+kakadu = "papousek"
+
+print(kakadu)
+print("kakadu")
