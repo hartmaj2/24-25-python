82401c5c832053382dbb96d55a6ce3afabbe78e5
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2024-09-27 14:15:31 +0200

    Add collatz problem

---
+++ 24_09_27/0Collatz/collatz.md
@@ -0,0 +1,10 @@
+# Collatz problem
+- We create numbers in order by using the collatz rules:
+  - pick your favorite number
+  - repeat forever:
+    - if 1 - finish (break)
+    - if even - divide by 2
+    - if odd - multiply by 3 and add 1
+
+- We don't know, if this procedure has to always end. Maybe there is a number for which it goes infinitely!
+- Try for number 64, try for 7
---
+++ 24_09_27/0Collatz/collatz.py
@@ -0,0 +1,9 @@
+# TODO: udelej program, ktery postupne tiskne collatzovu posloupnost od zadaneho cisla
+# jak funguje collatzova posloupnost?
+# 1. vyber si cislo a s tim zacnes
+# 2. zkontroluj zda je cislo sude, liche nebo 1
+#   (a) sude cislo -> vydel dvema, opakuj krok 2
+#   (b) liche cislo -> vynasob tremi, pak pricti 1 a opakuj krok 2
+#   (c) jednicka -> konec
+
+# TODO: soutez: kdo najde cislo s nejdelsi collatzovu posloupnosti, nez se zasekneme na 1
