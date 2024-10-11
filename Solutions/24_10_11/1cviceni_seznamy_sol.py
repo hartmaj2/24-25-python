5f206b48065414fee0c218d7703a7df57d0a294e
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2024-10-11 13:01:29 +0200

    Add solution for seznamy exercise

--- Solutions/24_10_11/1cviceni_seznamy_sol.py
+++ Solutions/24_10_11/1cviceni_seznamy_sol.py
@@ -5,6 +5,14 @@ store_items = ["mana potion","sword","armor","shield"]
 # tvuj inventar
 inventory = []
 
+def print_store_items():
+    print("Items in store: ")
+    for i in range(len(store_items)):
+        print(f"  {i+1}. {store_items[i]}") 
+
+def print_inventory():
+    print("Inventory: ",end=" ")
+    print(inventory)
 
 # TODO: vypis vsechny predmety, ktere obchod nabizi
 
@@ -15,15 +23,34 @@ inventory = []
 #   3. armor
 #   4. shield
 
+print("Items in store: ")
+for i in range(len(store_items)):
+    print(f"  {i+1}. {store_items[i]}")
+
 
 # TODO: kup si druhy predmet tim
 # 1. odeberes ten predmet ze seznamu predmetu `store_items` a pridas do sveho
 # 2. pridas tento predmet do seznamu `inventory`
 # POZOR: tvuj program by mel fungovat i v pripade, ze v obchodu budou uplne jine predmety
 
+print("\nTED SI NECO KOUPIM\n")
+
+item = store_items[1]
+store_items.remove(item)
+inventory.append(item)
+
+print_store_items()
+print_inventory()
 
 # TODO: Nech uzivatele zvolit cislo predmetu, ktery chce koupit a pridej mu ten predmet do inventare
 
+volba = input("Zadej cislo predmetu: ")
+item = store_items[int(volba)-1]
+store_items.remove(item)
+inventory.append(item)
+
+print_store_items()
+print_inventory()
 
 # BONUS: hezky design tohoto programu by byl takovy, 
 # ze pro vytisknuti veci v obchode bychom si napsali funkci
