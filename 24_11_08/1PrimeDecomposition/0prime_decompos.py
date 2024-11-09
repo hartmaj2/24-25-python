4340f6b04e207175224aa2735eb97a37ca133d0c
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2024-11-09 13:08:41 +0100

    Add prime decomposition final program

--- 24_11_08/1PrimeDecomposition/0prime_decompos.py
+++ 24_11_08/1PrimeDecomposition/0prime_decompos.py
@@ -1,1 +1,17 @@
 # Explain how is prime decomposition used in cryptography
+
+import sys
+
+number = int(input("Enter a number: "))
+divisor = 2
+rozklad = []
+
+while number != 1:
+    remainder = number % divisor
+    if remainder == 0:
+        number = number // divisor
+        rozklad.append(divisor)
+    else:
+        divisor += 1
+
+print(rozklad)
