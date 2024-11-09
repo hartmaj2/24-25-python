53a7b496041842dc497d7d1c7daa8789ba125bb5
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2024-11-09 13:09:18 +0100

    Polish code

--- 24_11_08/1PrimeDecomposition/0prime_decompos.py
+++ 24_11_08/1PrimeDecomposition/0prime_decompos.py
@@ -1,7 +1,5 @@
 # Explain how is prime decomposition used in cryptography
 
-import sys
-
 number = int(input("Enter a number: "))
 divisor = 2
 rozklad = []
@@ -9,7 +7,7 @@ rozklad = []
 while number != 1:
     remainder = number % divisor
     if remainder == 0:
-        number = number // divisor
+        number = number // divisor # the // is important here!
         rozklad.append(divisor)
     else:
         divisor += 1
