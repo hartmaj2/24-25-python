824ab45878a76a71496e1b512cee050c162307c7
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2024-11-10 20:59:29 +0100

    Add time checking

--- 24_11_08/1PrimeDecomposition/0prime_decompos.py
+++ 24_11_08/1PrimeDecomposition/0prime_decompos.py
@@ -1,8 +1,12 @@
 # Explain how is prime decomposition used in cryptography
 
+import time
+
 number = int(input("Enter a number: "))
 divisor = 2
 rozklad = []
+
+start = time.time()
 
 while number != 1:
     remainder = number % divisor
@@ -12,4 +16,5 @@ while number != 1:
     else:
         divisor += 1
 
+print(f"Elapsed time: {time.time()-start}")
 print(rozklad)
