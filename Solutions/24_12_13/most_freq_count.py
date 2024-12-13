f5de12dc35e10d8b7aee0a396da0c402a2e785be
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2024-12-13 14:27:10 +0100

    Add stuff so far

--- Solutions/24_12_13/most_freq_count.py
+++ Solutions/24_12_13/most_freq_count.py
@@ -23,41 +23,46 @@ def print_names():
 
 # TODO: How to find all unique names and accumulate them in a list
 
-def find_unique_names():
+def find_unique_names(filename):
     names = []
-    file = open("input.txt","r")
+    file = open(filename,"r")
     file.readline()
     for line in file:
         line = line.strip()
         if line not in names:
             names.append(line)
     file.close()
-    print(names)
+    return names
 
 # TODO: Load unique names and then calculate occurences, after that, find a maximum
 
-def calculate_name_frequencies():
+def calculate_name_frequencies(filename):
 
-    names = []
-    file = open("input.txt","r")
-    file.readline()
-    for line in file:
-        line = line.strip()
-        if line not in names:
-            names.append(line)
-    file.close()
+    names = find_unique_names(filename)
 
     counts = len(names) * [0]
-    file = open("input.txt","r")
+    file = open(filename,"r")
     file.readline()
     for line in file:
         line = line.strip()
         index = names.index(line)
         counts[index] += 1
     file.close()
-    print(names)
-    print(counts)
+    
+    return names, counts
         
 # TODO: Find index of maximum count name -> print the name and the count
 
-calculate_name_frequencies()
+def find_maximum(filename):
+
+    names, count = calculate_name_frequencies(filename)
+
+    maximum_index = 0
+
+    for i in range(len(names)):
+        if count[i] > count[maximum_index]:
+            maximum_index = i
+        
+    print(names[maximum_index],count[maximum_index])
+
+find_maximum("05.in")
