7bbdaede66fc7f7b0e2c8daa5beb1afd24e4eb61
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2024-12-16 16:18:44 +0100

    Add ksp problem

---
+++ 24_12_16/3KSP/0kebab.py
@@ -0,0 +1,5 @@
+# LINK: https://ksp.mff.cuni.cz/cviciste/?task=36-Z3-1:year=36
+
+# TODO: write a function to calculate price of kebab in a shop with given kebab price and distance from us
+
+# TODO: use the function to calculate minimum price over all possible kebab shops
