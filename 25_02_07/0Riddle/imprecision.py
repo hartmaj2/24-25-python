a232ee3ba473e769152169af67219aaaefd37786
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2025-02-07 13:49:21 +0100

    Add riddle

---
+++ 25_02_07/0Riddle/imprecision.py
@@ -0,0 +1,16 @@
+# What will the code print?
+
+increment = 0.1
+num = increment
+
+while num != 1.0:
+    print(f"The current number is {num}")
+    num += increment
+
+
+# increment = 1.0
+# num = increment
+
+# while num != 10.0:
+#     print(f"The current number is {num}")
+#     num += increment
