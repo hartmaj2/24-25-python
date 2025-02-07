7179d3eba736fba13f1acb06d5dff62966de6600
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2025-02-07 12:37:51 +0100

    Fix rotation

--- 25_01_24/1ZombieShooter/zombie_shooter.py
+++ 25_01_24/1ZombieShooter/zombie_shooter.py
@@ -5,8 +5,10 @@ pygame.init()
 
 # Screen setup
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+GAME_SCALE =  1
+
+SCREEN_WIDTH = 800 * GAME_SCALE
+SCREEN_HEIGHT = 600 * GAME_SCALE
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 game_clock = pygame.time.Clock()
 
@@ -16,7 +18,7 @@ BACKGROUND_COLOR = pygame.color.Color(60, 74, 55)
 
 # Player
 
-PLAYER_IMAGE = pygame.image.load("25_01_24/1ZombieShooter/player.svg")
+PLAYER_IMAGE = pygame.image.load("25_01_24/1ZombieShooter/player.svg").convert_alpha()
 PLAYER_IMAGE_CENTER_OFFSET = 14
 PLAYER_SPEED = 3
 
@@ -86,8 +88,8 @@ class Player:
         #         self.rotation += math.pi
 
         self.rotation = math.atan2(y_mouse_dist,x_mouse_dist)
-        
-        self.image = pygame.transform.rotate(PLAYER_IMAGE,360 * self.rotation / (2 * math.pi))
+        self.image = pygame.transform.rotozoom(PLAYER_IMAGE,360 * self.rotation / (2 * math.pi),1)
+        # self.image = pygame.transform.rotate(PLAYER_IMAGE,360 * self.rotation / (2 * math.pi)) // rotate distorts image when not rotated by increments of 90
 
         self.image_rect = self.image.get_rect()
         self.image_rect.centerx = (self.collider.centerx + PLAYER_IMAGE_CENTER_OFFSET * math.cos(self.rotation))
@@ -96,7 +98,7 @@ class Player:
 
 # Zombie
 
-ZOMBIE_IMAGE = pygame.image.load("25_01_24/1ZombieShooter/zombie.svg")
+ZOMBIE_IMAGE = pygame.image.load("25_01_24/1ZombieShooter/zombie.svg").convert_alpha()
 ZOMBIE_SPEED = 1
 
 class Zombie:
@@ -142,7 +144,8 @@ class Zombie:
         x_player_dist = player_pos[0]-self.x_pos
         y_player_dist = self.y_pos-player_pos[1]
         self.rotation = math.atan2(y_player_dist,x_player_dist)
-        self.image = pygame.transform.rotate(ZOMBIE_IMAGE,360 * self.rotation / (2 * math.pi))
+        self.image = pygame.transform.rotozoom(ZOMBIE_IMAGE,360 * self.rotation / (2 * math.pi),1)
+        # self.image = pygame.transform.rotate(ZOMBIE_IMAGE,360 * self.rotation / (2 * math.pi))
 
         self.image_rect = self.image.get_rect()
         self.image_rect.center = self.collider.center
@@ -159,7 +162,7 @@ def update():
     keys = pygame.key.get_pressed()
     mouse_pos = pygame.mouse.get_pos()
     player.update(keys,mouse_pos)
-    # zombie.update((player.collider.centerx,player.collider.centery))
+    zombie.update((player.collider.centerx,player.collider.centery))
 
 def render():
     global screen, player
