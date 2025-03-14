9f0984ed5f3929a0a4fb40714ac2daee0c695f0e
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2025-03-14 08:18:22 +0100

    Add preparation for lesson

--- 25_03_14/poker.py
+++ 25_03_14/poker.py
@@ -1,6 +1,48 @@
 from __future__ import annotations
+from enum import Enum, auto
 
 import random
+
+# poker combinations
+class Combination(Enum):
+    HIGH_CARD = auto()
+    ONE_PAIR = auto()
+    TWO_PAIRS = auto()
+    THREE_OF_KIND = auto()
+    STRAIGHT = auto()
+    FLUSH = auto()
+    FULL_HOUSE = auto()
+    FOUR_OF_KIND = auto()
+    STRAIGHT_FLUSH = auto()
+
+# combinations evaluation
+def check_flush(cards : list[Card]) -> bool:
+    suit = cards[0].suit
+    for card in cards:
+        if card.suit != suit:
+            return False
+    return True
+
+def check_straight(cards : list[Card]):
+    cards.sort(reverse=True)
+    firstval = cards[0].power
+    for i in range(len(cards)):
+        if cards[0].power != firstval + i:
+            return False
+    return True
+
+def check_n_of_kind(cards : list[Card], n : int):
+    cards.sort(reverse=True)
+    active_power = card[0]
+    amount_same = 1 # we have every time at least one same card to each other
+    for i in range(1,len(cards)):
+        if amount_same == n: # check if we have not encountered n same cards in a row
+            return True
+        if cards[i].power == active_power: # the next card has same power as the active power
+            amount_same += 1
+        else: # suddenly, we have a new card power (lower), so we reset the counter and start again
+            active_power = cards[i].power
+            amount_same = 1 
 
 class Card:
 
@@ -36,8 +78,22 @@ class Card:
     def __ge__(self, other : Card):
         return self > other or self == other
 
+class Player:
 
-    
+    def __init__(self, name : str, cards : list[Card]):
+        self.name = name
+        self.cards = cards
+
+    def __str__(self):
+        print(f"{self.name}")
+
+    # evaluation vector will be 6-tuple where the components of the tuple from left to right denote powers at different comparison levels
+    # (explain sorting using multiple runs going from the most specific to least specific tie breaking component)
+    def evaluate_hand(self, table_cards : list[Card]):
+        ...
+
+
+
 
 cards = []
 
@@ -52,4 +108,4 @@ cards.sort()
 for card in cards:
     print(card)
 
-
+# TODO: test the functions for combination evaluation
