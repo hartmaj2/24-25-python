4bf48660b1cc4aac6de784637b55e25bf2447fa9
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2025-06-06 11:31:34 +0200

    Add improved football player game

---
+++ 25_06_06/PlayerGuess/scraping_improved.py
@@ -0,0 +1,96 @@
+import os
+import urllib.request
+import random
+import time
+from http.client import HTTPResponse
+
+from typing import Callable
+
+import bs4
+
+
+class Player:
+    def __init__(self, name, team, nationality, position):
+        self.name = name
+        self.team = team
+        self.nationality = nationality
+        self.position = position
+
+
+    def __repr__(self):
+        return self.name
+
+
+def clear():
+    os.system("clear")
+
+
+def scrape_players() -> list[Player]:
+    odpoved : HTTPResponse = urllib.request.urlopen("https://www.givemesport.com/ballon-dor-power-rankings/")
+
+    polivka = bs4.BeautifulSoup(odpoved, "html.parser")
+
+    tables = polivka.find_all("table")
+    rows = tables[0].find_all("tr")
+    sloupce = rows[3].find_all("td")
+
+    players : list[Player] = []
+    for row in rows[2:]:
+
+        sloupce = row.find_all("td")
+        players.append(Player(sloupce[1].p.text, sloupce[2].p.text, sloupce[3].p.text, sloupce[4].p.text))
+
+    return players
+    
+clear()
+
+# functions Game_nationalities() and Game_Teams() look very similar
+# what are the differences?
+#   correct_answer is set based on nationality or team -> would be good to pass that information to the function from the outside
+#   different part of thext: Which [country/team] is -||--
+
+
+def play_game(players : list[Player], vybiratko : Callable[[Player],str], jmeno_vlastnosti : str):
+    points = 0
+    wait_time = 2
+    max_points = len(players)
+
+    for i in range(len(players)):
+        clear()
+        random_number = random.randint(0, len(players) - 1)
+        correct_answer = vybiratko(players[random_number]).lower()
+        if input(f"Hello! What is the {jmeno_vlastnosti} of {players[random_number]}? \n").lower() == correct_answer:
+            print("Thats correct!!!")
+            time.sleep(wait_time)
+            points += 1
+        else:
+            print(f"stupid idiot... Its {correct_answer}")
+            time.sleep(wait_time)
+
+        players.pop(random_number)
+
+
+    print(f"You got {points} points out of {max_points}") 
+
+
+def nationality_selector(player : Player) -> str:
+    return player.nationality
+
+def team_selector(player : Player) -> str:
+    return player.team
+
+def position_selector(player : Player) -> str:
+    return player.position
+
+players = scrape_players()
+
+game_wanted = input("Which game do you want to play? \n 1- Nationallity game \n 2- Team game \n 3- Position game \n")
+
+if game_wanted == "1":
+    play_game(players,nationality_selector,"nationality")
+
+elif game_wanted == "2":
+    play_game(players,team_selector,"team")
+
+elif game_wanted == "3":
+    play_game(players,position_selector,"position")
